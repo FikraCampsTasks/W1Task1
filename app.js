6f1367b05fc24b8177be01b1793425e0c26ba730
Salah Saleh <mr.salah1989@gmail.com>
2018-11-19 00:04:48 +0300

    2nd solution

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
-//Without Using any external library.
+//Using external library.
+const _ = require("underscore")
 fs.readFile("./flower.txt", (err, data) => {
   if(err) return;
-  const flowers = data.toString().split("\n");
-  console.log("Number of Rows: ", flowers.length);
-  const sFlower = flowers.filter(flower => flower[0] == "S");
-  console.log("List the flower name that start with letter 'S':", sFlower);
-  console.log("Count the number of flower that not start with letter 'S':",flowers.length - sFlower.length);
-  const aFlower = flowers.filter(flower => flower[0] == "A");
-  console.log("List the flower start with first letter of your name if your name start with 'S' use second letter:",sFlower);
-  const fiveFlower = flowers.filter(flower => flower.length == 5);
-  console.log("List all the flower the length of name is 5:", fiveFlower);
+  const flowers=data.toString().split("\n");
+  console.log("Number of Rows: ",flowers.length);
+  const sFlower = _.filter(flowers,(flower)=>flower[0]=="S")
+  console.log("List the flower name that start with letter 'S':",sFlower);
+  console.log("Count the number of flower that not start with letter 'S':",flowers.length-sFlower.length);
+  const aFlower = _.filter(flowers,(flower)=>flower[0]=="A")
+  console.log("List the flower start with first letter of your name if your name start with 'S' use second letter:",aFlower);
+  const fiveFlower=_.filter(flowers,(flower)=>flower.length==5)
+  console.log("List all the flower the length of name is 5:",fiveFlower);
 });
