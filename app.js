68fcfbb4fce3390ec1f52a3c72f71cd4a10c0f17
Salah Saleh <mr.salah1989@gmail.com>
2018-11-19 00:21:10 +0300

    3rd solution

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+// fs module to read and write files.
 const fs = require("fs");
-//Using external library.
-const _ = require("underscore")
+
+// readFile used to read file asynchronous.
 fs.readFile("./flower.txt", (err, data) => {
-  if(err) return;
-  const flowers=data.toString().split("\n");
-  console.log("Number of Rows: ",flowers.length);
-  const sFlower = _.filter(flowers,(flower)=>flower[0]=="S")
-  console.log("List the flower name that start with letter 'S':",sFlower);
-  console.log("Count the number of flower that not start with letter 'S':",flowers.length-sFlower.length);
-  const aFlower = _.filter(flowers,(flower)=>flower[0]=="A")
-  console.log("List the flower start with first letter of your name if your name start with 'S' use second letter:",aFlower);
-  const fiveFlower=_.filter(flowers,(flower)=>flower.length==5)
-  console.log("List all the flower the length of name is 5:",fiveFlower);
+  //Check if error stop execution by using return.
+  if (err) return;
+
+  //converting date to sting and using split to convert string to array.
+  const flowers = data.toString().split("\n");
+
+  // Assign number of row = length of array to the NumberOfRows constant.
+  const NumberOfRows = flowers.length;
+  console.log("Number of Rows: ", NumberOfRows);
+
+  // Create sFlower array by using filter method.
+  const sFlowers = flowers.filter(flower => flower[0] == "S");
+  console.log("List the flower name that start with letter 'S':", sFlowers);
+
+  // Remaining Flower equal to diffrence between tatal flowers length and sFlowers length
+  const remainingFlower = flowers.length - sFlowers.length;
+  console.log(
+    "Count the number of flower that not start with letter 'S':",
+    remainingFlower
+  );
+
+  // Create aFlower array by using filter method.
+  const aFlower = flowers.filter(flowers, flower => flower[0] == "A");
+  console.log(
+    "List the flower start with first letter of your name if your name start with 'S' use second letter:",
+    aFlower
+  );
+
+  // Create fiveLettersNameFlowers array by using filter method.
+  const fiveLettersNameFlowers = flowers.filter(flower => flower.length == 5);
+  console.log("List all the flower the length of name is 5:", fiveLettersNameFlowers);
 });
